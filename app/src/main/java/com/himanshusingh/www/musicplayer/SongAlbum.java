a0f5b7e35a21d32276bc3f09975c92094c7b12bc
NeoHimu <himanshus096@gmail.com>
2019-03-22 04:25:45 +0530

    album implementation is done

---
+++ app/src/main/java/com/himanshusingh/www/musicplayer/SongAlbum.java
@@ -0,0 +1,8 @@
+package com.himanshusingh.www.musicplayer;
+
+/**
+ * Created by himanshu on 20/3/19.
+ */
+
+public class SongAlbum {
+}
---
+++ app/src/main/java/com/himanshusingh/www/musicplayer/SwipeAdapter.java
@@ -0,0 +1,8 @@
+package com.himanshusingh.www.musicplayer;
+
+/**
+ * Created by himanshu on 21/3/19.
+ */
+
+public class SwipeAdapter {
+}
